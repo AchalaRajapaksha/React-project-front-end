ab34cad9b3a99110a642f7474b01ec932348e994
Achala Rajapaksha <achalarajapaksha01@gmail.com>
2023-12-19 02:33:29 +0530

    Chenging css to make screen responsive

--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -5,15 +5,41 @@ import Frame498 from './components/Frame498';
 import Frame499 from "./components/Frame499";
 import Herosection from "./components/Herosection";
 
+
 function App() {
-  return (
-    <div className="App">
-      <TopNavigation />
-        <Herosection />
-      <Frame499 />
-      <Frame498 />
-      <Footer />
-    </div>
+  return ( 
+      <div className="responsive-container">
+        <div className="content-for-wide-screens">
+          <TopNavigation />
+          <Herosection />
+          <Frame498 />
+          <Frame499 />
+          <Footer />
+        </div>
+        <div className="content-for-medium-screens">
+          <TopNavigation />
+          <Herosection />
+          <Frame498 />
+          <Frame499 />
+          <Footer />
+        </div>
+        <div className="content-for-small-screens">
+          <TopNavigation />
+          <Herosection />
+          <Frame498 />
+          <Frame499 />
+          <Footer />
+        </div>
+
+        <div className="content-for-mobile-screens">
+          <TopNavigation />
+          <Herosection />
+          <Frame498 />
+          <Frame499 />
+          <Footer />
+        </div>
+      </div>
+    
   );
 }
 
--- front-end-project/src/components/Herosection.js
+++ front-end-project/src/components/Herosection.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ReactComponent as Andykelly } from "./images/Andykelly.svg";
-import Textbox from "./components/Textbox";
+import Textbox from "./Textbox";
 import "./Herosection.css";
 
 export default function Herosection() {
   return (
-      <>
+    <>
+      <Textbox />
       <div className="Herosection">
         <Andykelly className="Andykelly" />
       </div>
-      <Textbox />
-      </>
-    );
+    </>
+  );
 }
