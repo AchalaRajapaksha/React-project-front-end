f5ce8a68f3601e56e4e4592396f3ca764f69e218
Achala Rajpaksha <achalarajapaksha01@gmail.com>
2023-12-18 14:56:55 +0530

    Footer created

--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TopNavigation from './components/TopNavigation';
 import Button from './components/Button';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <TopNavigation/>
       <Button/>
+      <Footer/>
     </div>
   );
 }
