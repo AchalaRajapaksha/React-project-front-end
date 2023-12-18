1f668b139624f89045e7b8a631a2cdd4404ce614
Achala Rajpaksha <achalarajapaksha01@gmail.com>
2023-12-18 12:11:36 +0530

    First page top nav bar and button created

---
+++ front-end-project/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <div className="Button">
+      <button className='button'>GET FREE CONSULTATION</button>
+    </div>
+  );
+};
+
+export default Button;
